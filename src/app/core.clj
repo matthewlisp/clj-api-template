71bf5ab9621ff119a86db14718b7b82b1ea8d0b6
matthewlisp <matthewlisp@gmail.com>
2019-09-10 15:49:26 -0300

    update handler to print terminal

--- src/app/core.clj
+++ src/app/core.clj
@@ -1,7 +1,9 @@
 (ns app.core
-  (:require [ring.adapter.jetty :refer [run-jetty]]))
+  (:require [ring.adapter.jetty :refer [run-jetty]]
+            [clojure.pprint :refer [pprint]]))
 
 (defn handler [request]
+  (clojure.pprint/pprint request) ;
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body "Hello world"})
