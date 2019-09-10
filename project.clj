79c9b9a6f0fe6a5e4858d3f89fd152756ea6f71e
matthewlisp <matthewlisp@gmail.com>
2019-09-10 17:07:16 -0300

    Updates compojure routes

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [ring "1.7.1"]]
+                 [ring "1.7.1"]
+                 [compojure "1.6.1"]]
   :repl-options {:init-ns app.core}
   :main app.core)
--- src/app/core.clj
+++ src/app/core.clj
@@ -1,13 +1,16 @@
 (ns app.core
   (:require [ring.adapter.jetty :refer [run-jetty]]
-            [clojure.pprint :refer [pprint]]))
+            [clojure.pprint     :refer [pprint]]
+            [compojure.core     :refer [routes GET]]
+            [compojure.route    :refer [not-found]]))
 
-(defn handler [request]
-  (clojure.pprint/pprint request) ;
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body "Hello world"})
+(def my-routes 
+  (routes ; routes function: Create a Ring handler by combining several handlers into one.
+   (GET  "/endpoint-a"  [] "<h1>Hello endpoint A</h1>")
+   (GET  "/endpoint-b"  [] "<h1>Hello endpoint B</h1>")
+   (not-found "<h1>Page not found</h1>")))
 
 (defn -main
+  "Main function executed when lein run command is used"
   []
-  (run-jetty handler {:port 3000}))
+  (run-jetty my-routes {:port 3000})) ; run-jetty is a ring adapter, used to convert ring handlers into running web servers.
