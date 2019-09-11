158973c6830faca64c617f0de455994b467da048
matthewlisp <matthewlisp@gmail.com>
2019-09-11 15:04:16 -0300

    update code to add json middlewares

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [ring "1.7.1"]
-                 [compojure "1.6.1"]]
+                 [compojure "1.6.1"]
+                 [ring/ring-json "0.5.0"]]
   :repl-options {:init-ns app.core}
   :main app.core)
